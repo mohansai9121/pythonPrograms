9592e74c1b6abd66622257e0a3defc96a5fb8322
Mohan Sai <mohansaichinthala9121@gmail.com>
2024-07-12 12:33:36 +0530

    Sorting numbers using quick sort with small changes

--- quick_sort.py
+++ quick_sort.py
@@ -7,22 +7,22 @@ def partition(arr, start_index, end_index):
     end1 = end_index
     pivot = arr[start_index]
     while start1 < end1:
-        while arr[start1] < pivot:
+        if arr[start1] < pivot:
             start1 += 1
-        while arr[end1] > pivot:
+        if arr[end1] > pivot:
             end1 -= 1
         if start1 < end1:
             arr[start1], arr[end1] = arr[end1], arr[start1]
-    arr[start_index] = arr[end1]
-    arr[end1] = pivot
+    if pivot >= arr[end1]:
+        arr[start_index], arr[end1] = arr[end1], pivot
     return end1
 
 
 def quick_sort(arr, start_index, end_index):
     if start_index < end_index:
         partition_index = partition(arr, start_index, end_index)
-        quick_sort(arr, start_index, partition_index - 1)
-        quick_sort(arr, partition_index + 1, end_index)
+        quick_sort(arr, start_index, partition_index-1)
+        quick_sort(arr, partition_index+1, end_index)
 
 
 quick_sort(numbers, 0, len(numbers) - 1)
